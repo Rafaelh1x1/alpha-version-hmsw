16cf857156e8cf3ee40ba1929b33d19ecb0936ed
Rafael Hung <111296938+Rafaelh1x1@users.noreply.github.com>
2023-05-08 16:57:43 -0400

    Begin Halfmoon seafood project

---
+++ src/topBanner.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function TopBanner() {
+  return (
+    <div>
+      <p className="top-banner">
+        11508 N 56TH STREET, TEMPLE TERRACE, FL 33617 AT813-437-3474
+      </p>
+    </div>
+  );
+}
+
+export default TopBanner;
