f694982f69e739d0a33fd6ee85ecd34564d04ed7
Rafael Hung <111296938+Rafaelh1x1@users.noreply.github.com>
2023-05-10 09:04:43 -0400

    added all images to teh best seller section

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from "./logo.svg";
 import "./App.css";
 import TopBanner from "./topBanner";
 import BestSellers from "./bestSellers";
-import SubHeading from "./subHeading";
+import SubHeading from "./SubHeading";
 import CrabCakeSand from "./images/CrabCakeSand.jpg";
 import FishNChips from "./images/FishNChips.jpg";
 import FishNShrimp from "./images/FishNShrimp.jpg";
@@ -23,6 +23,16 @@ function App() {
           imageSrc={FishNChips}
           imageAlt="Fish and Chips"
           text="Fish and Chips"
+        ></BestSellers>
+        <BestSellers
+          imageSrc={FishNShrimp}
+          imageAlt="Fish and Shrimp"
+          text="Fish and Shrimp"
+        ></BestSellers>
+        <BestSellers
+          imageSrc={HotTossChik}
+          imageAlt="Hot Tossed Chicken with Collard Greens"
+          text="Hot Toss CHicken with Collard Greens"
         ></BestSellers>
       </div>
     </div>
