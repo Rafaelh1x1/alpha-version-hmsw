6861c2ae029ae6c2038c8eca53ba3e5d6aec67b1
Rafael Hung <hungrafael37@gmail.com>
2023-05-24 23:25:10 -0400

    New image for top banner + new best sellers layout

--- src/topBanner.js
+++ src/topBanner.js
@@ -1,10 +1,16 @@
 import React from "react";
-import topviewfood from "./images/topviewfood1.jpg";
+import topviewfood from "./images/topviewfood2.jpg";
 function TopBanner(props) {
   return (
     <div>
       <p className="top-banner">{props.text}</p>
-      <img src={topviewfood} alt="top view of food" class="top-banner-img" />
+      <div class="top-banner-img-container">
+        <img src={topviewfood} alt="top view of food" class="top-banner-img" />
+        <div class="text-overlay">
+          <p>HALF MOON SEAFOOD</p>
+          <p>Sustainably Sourced</p>
+        </div>
+      </div>
     </div>
   );
 }
