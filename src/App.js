7757f4ca75b1d9676f825b38e7fc8643c5043979
Rafael Hung <111296938+Rafaelh1x1@users.noreply.github.com>
2023-06-01 20:53:32 -0400

    Attempting to add functionality

--- src/App.js
+++ src/App.js
@@ -57,47 +57,94 @@ function App() {
         <ChooseLocation text="Sparkman Wharf"></ChooseLocation>
       </div>
 
-      {/* Click these and they will change the HTML being displaeyd */}
-      <MenuItemSubheading></MenuItemSubheading>
+      <div className="menu-Temple hidden">
+        {/* Click these and they will change the HTML being displaeyd */}
+        <MenuItemSubheading></MenuItemSubheading>
 
-      {/* This is where all the diffrent menu items go */}
+        {/* This is where all the diffrent menu items go */}
 
-      {/* this is where all the images for the various items will be */}
-      <div className="menu-type-container">
-        <div>
-          <MenuItemImg
-            imageSrc={FishNChips}
-            imageAlt="Fish and chips"
-          ></MenuItemImg>
+        {/* this is where all the images for the various items will be */}
+        <div className="menu-type-container">
+          <div>
+            <MenuItemImg
+              imageSrc={FishNChips}
+              imageAlt="Fish and chips"
+            ></MenuItemImg>
+          </div>
+          {/* this is where all the types of menu item go */}
+          <div>
+            <MenuItemType type="Plates"></MenuItemType>
+
+            {/* this is where all the actual menu items go  */}
+            <MenuItem
+              itemName="FISH & CHIPS"
+              itemDescription="1/2 lb of your choice of fish, served with tartar sauce and shoestring fries."
+              itemPrice=" Cod 15 | Gulf Grouper per market Price | Mississippi Catfish 16"
+            ></MenuItem>
+
+            <MenuItem
+              itemName="SHRIMP & FISH"
+              itemDescription="Your choice of fish, shrimp and a side. Served with house tartar and cocktail sauce."
+              itemPrice="Cod 19 | Gulf Grouper per Market Price | Mississippi Catfish 20"
+            ></MenuItem>
+
+            <MenuItem
+              itemName="SAMPLE THE SEA"
+              itemDescription="Try it all! Lightly battered shrimp, fish, and your choice of oysters or a house-made crab cake. Served with two sides."
+              itemPrice="Cod 28 | Gulf Grouper per Market Price | Mississippi Catfish 29"
+            ></MenuItem>
+
+            <MenuItem
+              itemName="SHRIMP & CHIPS"
+              itemDescription="1/2 lb of wild-caught shrimp, served with house cocktail sauce and a side."
+              itemPrice="16"
+            ></MenuItem>
+          </div>
         </div>
-        {/* this is where all the types of menu item go */}
-        <div>
-          <MenuItemType type="Plates"></MenuItemType>
+      </div>
+      <div className="menu-sparkman hidden">
+        {/* Click these and they will change the HTML being displaeyd */}
+        <MenuItemSubheading></MenuItemSubheading>
 
-          {/* this is where all the actual menu items go  */}
-          <MenuItem
-            itemName="FISH & CHIPS"
-            itemDescription="1/2 lb of your choice of fish, served with tartar sauce and shoestring fries."
-            itemPrice=" Cod 15 | Gulf Grouper per market Price | Mississippi Catfish 16"
-          ></MenuItem>
+        {/* This is where all the diffrent menu items go */}
 
-          <MenuItem
-            itemName="SHRIMP & FISH"
-            itemDescription="Your choice of fish, shrimp and a side. Served with house tartar and cocktail sauce."
-            itemPrice="Cod 19 | Gulf Grouper per Market Price | Mississippi Catfish 20"
-          ></MenuItem>
+        {/* this is where all the images for the various items will be */}
+        <div className="menu-type-container">
+          <div>
+            <MenuItemImg
+              imageSrc={FishNChips}
+              imageAlt="Fish and chips"
+            ></MenuItemImg>
+          </div>
+          {/* this is where all the types of menu item go */}
+          <div>
+            <MenuItemType type="Plates"></MenuItemType>
 
-          <MenuItem
-            itemName="SAMPLE THE SEA"
-            itemDescription="Try it all! Lightly battered shrimp, fish, and your choice of oysters or a house-made crab cake. Served with two sides."
-            itemPrice="Cod 28 | Gulf Grouper per Market Price | Mississippi Catfish 29"
-          ></MenuItem>
+            {/* this is where all the actual menu items go  */}
+            <MenuItem
+              itemName="FISH & CHIPS"
+              itemDescription="1/2 lb of your choice of fish, served with tartar sauce and shoestring fries."
+              itemPrice=" Cod 15 | Gulf Grouper per market Price | Mississippi Catfish 16"
+            ></MenuItem>
 
-          <MenuItem
-            itemName="SHRIMP & CHIPS"
-            itemDescription="1/2 lb of wild-caught shrimp, served with house cocktail sauce and a side."
-            itemPrice="16"
-          ></MenuItem>
+            <MenuItem
+              itemName="SHRIMP & FISH"
+              itemDescription="Your choice of fish, shrimp and a side. Served with house tartar and cocktail sauce."
+              itemPrice="Cod 19 | Gulf Grouper per Market Price | Mississippi Catfish 20"
+            ></MenuItem>
+
+            <MenuItem
+              itemName="SAMPLE THE SEA"
+              itemDescription="Try it all! Lightly battered shrimp, fish, and your choice of oysters or a house-made crab cake. Served with two sides."
+              itemPrice="Cod 28 | Gulf Grouper per Market Price | Mississippi Catfish 29"
+            ></MenuItem>
+
+            <MenuItem
+              itemName="SHRIMP & CHIPS"
+              itemDescription="1/2 lb of wild-caught shrimp, served with house cocktail sauce and a side."
+              itemPrice="16"
+            ></MenuItem>
+          </div>
         </div>
       </div>
       <SubHeading text="HOURS & LOCATIONS"></SubHeading>
