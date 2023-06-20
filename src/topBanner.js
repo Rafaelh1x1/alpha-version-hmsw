dcdaa0cfa06d178debe4c05a6d6f10cc6237fb29
Rafael Hung <111296938+Rafaelh1x1@users.noreply.github.com>
2023-06-20 08:04:57 -0400

    Mobile Nav is working

--- src/topBanner.js
+++ src/topBanner.js
@@ -1,7 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import QuickNav from "./QuickNav";
 import topviewfood from "./images/topviewfood2.jpg";
+
 function TopBanner(props) {
+  const [isNavOpen, setIsNavOpen] = useState(false);
+
+  const handleNavToggle = () => {
+    setIsNavOpen(!isNavOpen);
+  };
+
   return (
     <div>
       <p className="top-banner">{props.text}</p>
@@ -10,16 +17,17 @@ function TopBanner(props) {
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
           viewBox="0 0 24 24"
-          stroke-width="1.5"
+          strokeWidth="1.5"
           stroke="currentColor"
           width="24px"
           height="24px"
-          class="open-nav"
+          className={`open-nav ${isNavOpen ? "hidden" : ""}`}
+          onClick={handleNavToggle}
         >
           <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            d="M9 12.75l3 3m0 0l3-3m-3 3v-7.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
           />
         </svg>
 
@@ -27,26 +35,33 @@ function TopBanner(props) {
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
           viewBox="0 0 24 24"
-          stroke-width="1.5"
+          strokeWidth="1.5"
           stroke="currentColor"
           height="24px"
           width="24px"
-          class="close-nav"
+          className={`close-nav ${isNavOpen ? "" : "hidden"}`}
+          onClick={handleNavToggle}
         >
           <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
+            strokeLinecap="round"
+            strokeLinejoin="round"
             d="M9.75 9.75l4.5 4.5m0-4.5l-4.5 4.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
           />
         </svg>
       </div>
-      <QuickNav></QuickNav>
-      <div class="top-banner-img-container">
-        <img src={topviewfood} alt="top view of food" class="top-banner-img" />
-        <div class="text-overlay">
-          <p class="top-banner-img-bold-text">HALF MOON </p>
-          <p class="top-banner-img-bold-text">Seafood Co.</p>
-          <p class="top-banner-img-ss-text">Sustainably Sourced seafood</p>
+
+      {isNavOpen && <QuickNav />}
+
+      <div className="top-banner-img-container">
+        <img
+          src={topviewfood}
+          alt="top view of food"
+          className="top-banner-img"
+        />
+        <div className="text-overlay">
+          <p className="top-banner-img-bold-text">HALF MOON</p>
+          <p className="top-banner-img-bold-text">Seafood Co.</p>
+          <p className="top-banner-img-ss-text">Sustainably Sourced seafood</p>
         </div>
       </div>
     </div>
